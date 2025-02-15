dbbcf74e859d2ce21f587d9a123e3c3b3f264621
Verfinity <129280114+Verfinity@users.noreply.github.com>
2025-02-15 17:21:24 +0300

    Added unit tests for UsersController

---
+++ ChatsWebApi/ChatsWebApiTests/UsersControllerTest/GetUsers.cs
@@ -0,0 +1,39 @@
+﻿using ChatsWebApi.Components.Controllers;
+using ChatsWebApi.Components.Repositories.Users;
+using ChatsWebApi.Components.Types.Database;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ChatsWebApiTests.UsersControllerTest
+{
+    public class GetUsers
+    {
+        [Fact]
+        public async Task Should_Returns_No_Content()
+        {
+            var mock = new Mock<IUsersRepository>();
+            mock.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<User>());
+            var usersController = new UsersController(mock.Object);
+
+            var answer = await usersController.GetUsersAsync();
+
+            Assert.IsType<NoContentResult>(answer.Result);
+        }
+
+        [Fact]
+        public async Task Should_Returns_List_Of_Objects()
+        {
+            var testUsers = UsersHelper.GetTestUsers();
+            var mock = new Mock<IUsersRepository>();
+            mock.Setup(repo => repo.GetAllAsync()).ReturnsAsync(testUsers);
+            var usersController = new UsersController(mock.Object);
+
+            var answer = await usersController.GetUsersAsync();
+
+            var actionResult = Assert.IsType<ActionResult<List<User>>>(answer);
+            var okResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+            var result = Assert.IsType<List<User>>(okResult.Value);
+            Assert.Equal(testUsers, result);
+        }
+    }
+}
---
+++ ChatsWebApi/ChatsWebApiTests/UsersControllerTest/UsersHelper.cs
@@ -0,0 +1,35 @@
+﻿using ChatsWebApi.Components.Types.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatsWebApiTests.UsersControllerTest
+{
+    public static class UsersHelper
+    {
+        public static List<User> GetTestUsers()
+        {
+            return new List<User>
+            {
+                new User
+                {
+                    Id = 1,
+                    FirstName = "AA",
+                    LastName = "AA",
+                    NickName = "AA",
+                    Password = "12345678"
+                },
+                new User
+                {
+                    Id = 2,
+                    FirstName = "BB",
+                    LastName = "BB",
+                    NickName = "BB",
+                    Password = "12345678"
+                }
+            };
+        }
+    }
+}
